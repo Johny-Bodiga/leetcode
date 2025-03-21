9309c91fb03bd75b1eb6c8acf38780f10c3587f8
Johny Bodiga <163446176+Johny-Bodiga@users.noreply.github.com>
2025-03-21 18:26:38 +0530

    Time: 0 ms (100%), Space: 17.8 MB (38.35%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,15 +1,17 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        c=100
-        while c>1:
-            c=0
+        l=[]
+        
+        while True:
             n1=0
             while n>0:
-                c+=1
                 n1+=(n%10)*(n%10)
                 n=n//10
             n=n1
             if n1==1:
                 return True
+            if n1 in l:
+                return False
+            l.append(n1)
         return False
         
