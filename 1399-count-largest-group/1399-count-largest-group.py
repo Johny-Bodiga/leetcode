275a3fe250b9e8d9003f93be4269f1e53b0b75ac
Johny Bodiga <163446176+Johny-Bodiga@users.noreply.github.com>
2025-04-23 13:32:49 +0530

    Time: 20 ms (84%), Space: 17.6 MB (90.25%) - LeetHub

---
+++ 1399-count-largest-group/1399-count-largest-group.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        l=[0]*40
+        for i in range(1,n+1):
+            c=0
+            num=i
+            while num>0:
+                c+=num%10
+                num//=10
+            l[c]+=1
+        m=max(l)
+        ans=0
+        for i in l:
+            if i==m:
+                ans+=1
+        return ans
+
